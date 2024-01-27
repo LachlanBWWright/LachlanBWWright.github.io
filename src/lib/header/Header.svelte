28760a261beb32ccdd78d35ced0b6d93d630bb8f
LachlanBWWright <lachlanbwwright@gmail.com>
2024-01-27 23:40:08 +1100

    Navbar development

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,4 +1,6 @@
 <div class="p-2 text-center">
     <p class="text-2xl sm:text-4xl md:text-6xl font-bold">Lachlan Wright</p>
-    <p>Aspiring Full-Stack Developer</p>
+    <p class="text-lg sm:text-2xl lg:text-4xl">Aspiring Full-Stack Developer</p>
+    <br/>
+    <p><a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.github.com/LachlanBWWright">GitHub Profile</a> | <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.linkedin.com/in/LachlanBWWright/">LinkedIn Profile</a></p>
 </div>
--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -1,12 +1,34 @@
-<script lang="ts">
-    export let test: number;
-    let selectedValue = 0;
+<script>
+    let dropdownExpanded = false;
+
+    function openDropdown() {
+        dropdownExpanded = true;
+    }
+
+    function closeDropdown() {
+        dropdownExpanded = false;
+    }
 </script>
 
-<div class='sticky top-0 flex flex-row gap-2 p-1 w-full bg-gray-900 text-white text-2xl'>
-    <a class="hover:text-white" href="#test">Test</a>
-    <a class="hover:text-white" href="#test">Test</a>
-    <a class="hover:text-white" href="#test">Test</a>
-    <a class="hover:text-white" href="#test">Test</a>
-    {test}
-</div>
+<nav class='sticky top-0 flex flex-row items-center gap-4 p-1 w-full bg-[#222222] text-white text-md lg:text-xl xl:text-2xl'>
+    {#if dropdownExpanded}
+    <button class="sm:hidden aspect-square rounded-md border-2 px-2 border-white" on:click={closeDropdown}>=</button>
+    {:else}
+    <button class="sm:hidden aspect-square rounded-md border-2 px-2 border-white" on:click={openDropdown}>=</button>
+    {/if}
+    <a class="hover:text-blue-500 font-bold" href="#test">Lachlan Wright</a>
+    <a class="hover:text-blue-500 hidden sm:block" href="#test">Competencies</a>
+    <a class="hover:text-blue-500 hidden sm:block" href="#test">Experience</a>
+    <a class="hover:text-blue-500 hidden sm:block" href="#test">Portfolio</a>
+    <a class="hover:text-blue-500 hidden sm:block" href="#test">Resume</a>
+    <a class="hover:text-blue-500 hidden sm:block" href="#test">Academic Record</a>
+    {#if dropdownExpanded}
+        <div class="flex flex-col absolute left-0 top-[37px] w-screen bg-[#333333] sm:hidden z-10">
+                <a class="hover:text-blue-500 sm:block" href="#test">Competencies</a>
+            <a class="hover:text-blue-500 sm:block" href="#test">Experience</a>
+            <a class="hover:text-blue-500 sm:block" href="#test">Portfolio</a>
+            <a class="hover:text-blue-500 sm:block" href="#test">Resume</a>
+            <a class="hover:text-blue-500 sm:block" href="#test">Academic Record</a>
+        </div>
+    {/if}
+</nav>
