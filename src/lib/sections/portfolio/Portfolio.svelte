352a6e9ca9b4a80508cfd42d42b7a922dc6f5e71
Lachlan Wright <79518204+LachlanBWWright@users.noreply.github.com>
2024-02-11 21:32:07 +1100

    Add placeholder portfolio card

--- src/lib/sections/portfolio/Portfolio.svelte
+++ src/lib/sections/portfolio/Portfolio.svelte
@@ -1,29 +1,36 @@
+<script>
+  import PortfolioCard from "$lib/items/PortfolioCard.svelte";
+  import PortfolioItemHeader from "$lib/items/PortfolioItemHeader.svelte";
+
+</script>
 <div class="flex flex-col gap-2">
     <p class="text-3xl mx-auto">Portfolio</p>
     <div class="grid grid-cols-1 grid-rows-2 gap-1 md:mx-10 lg:mx-40">
-        <div>            
-            <p class="text-2xl text-center">Item</p>
-            <ul class="list-disc pl-5">
-                <li>Description</li>
-            </ul>
-        </div>
-        <div>            
-            <p class="text-2xl text-center">Item</p>
-            <ul class="list-disc pl-5">
-                <li>Description</li>
-            </ul>
-        </div>
-        <div>            
-            <p class="text-2xl text-center">Item</p>
-            <ul class="list-disc pl-5">
-                <li>Description</li>
-            </ul>
-        </div>
-        <div>            
-            <p class="text-2xl text-center">Item</p>
-            <ul class="list-disc pl-5">
-                <li>Description</li>
-            </ul>
-        </div>
+        <PortfolioCard>
+            <PortfolioItemHeader>
+                <p>Placeholder</p>
+            </PortfolioItemHeader>
+        </PortfolioCard>
+        <PortfolioCard>
+            <PortfolioItemHeader>
+                <p>Placeholder</p>
+            </PortfolioItemHeader>
+        </PortfolioCard>
+        <PortfolioCard>
+            <PortfolioItemHeader>
+                <p>Placeholder</p>
+            </PortfolioItemHeader>
+        </PortfolioCard>
+        <PortfolioCard>
+            <PortfolioItemHeader>
+                <p>Placeholder</p>
+            </PortfolioItemHeader>
+        </PortfolioCard>
+        <PortfolioCard>
+            <PortfolioItemHeader>
+                <p>Placeholder</p>
+            </PortfolioItemHeader>
+        </PortfolioCard>
+
     </div>
 </div>
